d9afd69baa9278b92b632c37dcb9507487fe4bfc
Mason Fidino <mfidino@lpzoo.org>
2019-05-03 09:28:10 -0500

    removing unnessecary code

--- sourcer.R
+++ sourcer.R
@@ -2,6 +2,7 @@
 # scripts to source
 source("utility_script.R")
 source("plotting_utility.R")
+source("prepare_data.R")
 
 # required packages
 packs <- c("dplyr", "reshape2", "runjags", "MCMCpack", "mcmcplots",
