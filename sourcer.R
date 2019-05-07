baa0f3b9b69d4368e5c5f866315bd9426b6bf122
Mason Fidino <mfidino@lpzoo.org>
2019-05-07 08:57:57 -0500

    updated sourcer

--- sourcer.R
+++ sourcer.R
@@ -2,10 +2,12 @@
 # scripts to source
 source("utility_script.R")
 source("plotting_utility.R")
-source("prepare_data.R")
+
 
 # required packages
 packs <- c("dplyr", "reshape2", "runjags", "MCMCpack", "mcmcplots",
-           'parallel', "coda", "stringr", "data.table", "DMwR")
+           'parallel', "coda", "stringr", "data.table", "DMwR", 'coda')
 
 package_load(packs)
+
+source("prepare_data.R")
